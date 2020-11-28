b2cdc2d7488df08b241253d327b4fd884f46fe14
m-shaka <buddha0818@gmail.com>
2020-11-28 22:39:28 +0900

    chap2

---
+++ src/bin/chap2.rs
@@ -0,0 +1,22 @@
+use ray_tracer::vec3;
+use std::io::{self, Write};
+
+fn main() {
+    let mut stdout = io::stdout();
+    let nx = 200;
+    let ny = 100;
+    write!(stdout, "P3\n{} {}\n255\n", nx, ny).unwrap();
+    for j in (0..ny).rev() {
+        for i in 0..nx {
+            let col = vec3::Vec3 {
+                x: i as f32 / nx as f32,
+                y: j as f32 / ny as f32,
+                z: 0.2,
+            };
+            let ir = (255.99 * col.x) as u32;
+            let ig = (255.99 * col.y) as u32;
+            let ib = (255.99 * col.z) as u32;
+            write!(stdout, "{} {} {}\n", ir, ig, ib).unwrap();
+        }
+    }
+}
---
+++ src/vec3.rs
@@ -0,0 +1,182 @@
+use std::ops;
+
+#[derive(Clone, Copy)]
+pub struct Vec3 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+impl Vec3 {
+    #[inline]
+    pub fn length(&self) -> f32 {
+        self.squared_length().sqrt()
+    }
+
+    #[inline]
+    fn squared_length(&self) -> f32 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    #[inline]
+    pub fn make_unit_vector(&self) -> Self {
+        let k = 1.0 / self.length();
+        *self * k
+    }
+
+    #[inline]
+    pub fn dot(&self, other: Self) -> f32 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
+    #[inline]
+    pub fn cross(&self, other: Self) -> Self {
+        Vec3 {
+            x: self.y * other.z - self.z * other.y,
+            y: -(self.x * other.z - self.z * other.x),
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
+}
+
+impl ops::Neg for Vec3 {
+    type Output = Self;
+    fn neg(self) -> Self {
+        Self {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z,
+        }
+    }
+}
+
+impl ops::Add for Vec3 {
+    type Output = Self;
+
+    #[inline]
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+impl ops::Sub for Vec3 {
+    type Output = Self;
+
+    #[inline]
+    fn sub(self, other: Self) -> Self {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+impl ops::Mul for Vec3 {
+    type Output = Self;
+    #[inline]
+    fn mul(self, other: Self) -> Self {
+        Self {
+            x: self.x * other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+impl ops::Mul<f32> for Vec3 {
+    type Output = Self;
+    #[inline]
+    fn mul(self, scalar: f32) -> Self {
+        Self {
+            x: self.x * scalar,
+            y: self.y + scalar,
+            z: self.z + scalar,
+        }
+    }
+}
+
+impl ops::Div for Vec3 {
+    type Output = Self;
+    #[inline]
+    fn div(self, other: Self) -> Self {
+        Self {
+            x: self.x / other.x,
+            y: self.y / other.y,
+            z: self.z / other.z,
+        }
+    }
+}
+
+impl ops::Div<f32> for Vec3 {
+    type Output = Self;
+    #[inline]
+    fn div(self, scalar: f32) -> Self {
+        Self {
+            x: self.x / scalar,
+            y: self.y / scalar,
+            z: self.z / scalar,
+        }
+    }
+}
+
+impl ops::AddAssign for Vec3 {
+    #[inline]
+    fn add_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        };
+    }
+}
+
+impl ops::SubAssign for Vec3 {
+    #[inline]
+    fn sub_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        };
+    }
+}
+impl ops::MulAssign for Vec3 {
+    #[inline]
+    fn mul_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x * other.x,
+            y: self.y * other.y,
+            z: self.z * other.z,
+        };
+    }
+}
+
+impl ops::DivAssign for Vec3 {
+    #[inline]
+    fn div_assign(&mut self, other: Self) {
+        *self = Self {
+            x: self.x / other.x,
+            y: self.y / other.y,
+            z: self.z / other.z,
+        };
+    }
+}
+
+impl ops::MulAssign<f32> for Vec3 {
+    #[inline]
+    fn mul_assign(&mut self, scalar: f32) {
+        *self = *self * scalar;
+    }
+}
+
+impl ops::DivAssign<f32> for Vec3 {
+    #[inline]
+    fn div_assign(&mut self, scalar: f32) {
+        *self = *self / scalar;
+    }
+}
