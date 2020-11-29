3ca8bded7646168b07e0fb4090541ac0be106d90
m-shaka <buddha0818@gmail.com>
2020-11-29 09:54:47 +0900

    refactor: use Vec3::new

--- src/bin/chap2.rs
+++ src/bin/chap2.rs
@@ -1,4 +1,4 @@
-use ray_tracer::vec3;
+use ray_tracer::vec3::Vec3;
 use std::io::{self, Write};
 
 fn main() {
@@ -8,11 +8,7 @@ fn main() {
     write!(stdout, "P3\n{} {}\n255\n", nx, ny).unwrap();
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let col = vec3::Vec3 {
-                x: i as f32 / nx as f32,
-                y: j as f32 / ny as f32,
-                z: 0.2,
-            };
+            let col = Vec3::new(i as f32 / nx as f32, j as f32 / ny as f32, 0.2);
             let ir = (255.99 * col.x) as u32;
             let ig = (255.99 * col.y) as u32;
             let ib = (255.99 * col.z) as u32;
